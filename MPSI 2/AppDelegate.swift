6e52459c2faea6e66c6d7104811f64147d9a61cc
TheModernEra <bsrosenthal101@gmail.com>
2020-08-05 17:03:11 -0700

    Initial Commit

---
+++ MPSI 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MPSI 2
+//
+//  Created by Bennett Rosenthal on 8/5/20.
+//  Copyright © 2020 ModernEra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MPSI 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MPSI 2
+//
+//  Created by Bennett Rosenthal on 8/5/20.
+//  Copyright © 2020 ModernEra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
