b1f63df1c2173692eb60a3f881a1a74dbda4077e
TheModernEra <bsrosenthal101@gmail.com>
2020-08-06 00:03:57 -0700

    added permissions button functionality

--- MPSI 2/ViewController.swift
+++ MPSI 2/ViewController.swift
@@ -11,9 +11,11 @@ import Alamofire
 import SSZipArchive
 
 class ViewController: NSViewController {
-
+    let defaultMessage = "Welcome to MPSI. First, click the Download Game button to download Pavlov: Shack."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        installationLabel.stringValue = "\(defaultMessage)"
         // Do any additional setup after loading the view.
     }
 
@@ -35,7 +37,30 @@ class ViewController: NSViewController {
     }
     
     @IBAction func permsButtonPressed(_ sender: Any) {
-    
+        let stringPath = Bundle.main.path(forResource: "adb", ofType: "")
+        
+        @discardableResult
+        func shell(_ args: String...) -> Int32 {
+            let task = Process()
+            task.launchPath = stringPath
+            task.arguments = args
+            task.launch()
+            task.waitUntilExit()
+            return task.terminationStatus
+        }
+        installationLabel.stringValue = "Setting permissions..."
+        Dispatch.background {
+            _ = shell("-d", "shell", "pm", "grant", "com.vankrupt.pavlov", "android.permission.RECORD_AUDIO")
+            _ = shell("-d", "shell", "pm", "grant", "com.vankrupt.pavlov", "android.permission.READ_EXTERNAL_STORAGE")
+            _ = shell("-d", "shell", "pm", "grant", "com.vankrupt.pavlov", "android.permission.WRITE_EXTERNAL_STORAGE")
+            Dispatch.main {
+                self.installationLabel.stringValue = "Permissions set!"
+                let seconds = 3.0
+                DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+                    self.installationLabel.stringValue = "\(self.defaultMessage)"
+                }
+            }
+        }
     }
     
     @IBAction func nameButtonPressed(_ sender: Any) {
