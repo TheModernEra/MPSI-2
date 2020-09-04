22d2a2dd389f04050b7ddb70850b8f3e44ea1de3
TheModernEra <bsrosenthal101@gmail.com>
2020-09-03 23:43:21 -0700

    deletes upsiopts.txt on shutdown now to clean things up

--- MPSI 2/AppDelegate.swift
+++ MPSI 2/AppDelegate.swift
@@ -18,6 +18,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        let usernameFilePath = NSString(string: "~").expandingTildeInPath
+        let txtPath = NSString(string: "~/Downloads/upsiopts.txt").expandingTildeInPath
+        let txtDoesExist = FileManager.default.fileExists(atPath: txtPath)
+        if txtDoesExist == true {
+            do {
+                try FileManager.default.removeItem(atPath: "\(usernameFilePath)/Downloads/upsiopts.txt")
+            }
+            catch {
+                print(error)
+            }
+        }
     }
 
 
